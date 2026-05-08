a7ee0b544526a75e7db9efb44b357d51b1f53241
ijhar-123 <123933534+ijhar-123@users.noreply.github.com>
2026-05-09 02:09:06 +0530

    8-bit encoder to use assign and case statement

--- lab/encoder_8bit.v
+++ lab/encoder_8bit.v
@@ -1,1 +1,38 @@
 
+//using assign
+/*module encoder_8bit (
+    output [2:0]y,
+    input [7:0]a
+
+);
+assign y2 = a[7] | a[6] | a[5] | a[4];
+assign y1 = a[7] | a[6] | a[3] | a[2];
+assign y0 = a[7] | a[5] | a[3] | a[1];
+
+
+    
+endmodule*/
+
+//using case
+module encoder_8bit (
+    input [7:0]a,
+    output reg [2:0]y
+);
+
+always @(*) begin
+    case (a)
+      8'b00000001  : y = 3'b000;
+      8'b00000010  : y = 3'b001;
+      8'b00000100  : y = 3'b010;
+      8'b00001000  : y = 3'b011;
+      8'b00010000  : y = 3'b100;
+      8'b00100000  : y = 3'b101;
+      8'b01000000  : y = 3'b110;
+      8'b10000000  : y = 3'b111;
+        default:     y = 3'bxxx;
+    endcase
+        
+    
+    
+end
+endmodule
